5a6b1734282a1cc9af8522b4afb53aef6ba5572f
SenpaiTn <khaledsahli36@gmail.com>
2022-02-27 12:34:10 +0100

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const port = process.env.PORT || 5000;
-const publicPath = path.join(__dirname, "..", "public");
 
 require("dotenv").config();
 
@@ -28,7 +27,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
